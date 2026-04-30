52d32386cb609ea1f966d486db3c6c0b29a64ae2
nokamoto-cyber <nokamoto@sejuku.net>
2026-04-30 14:58:57 +0900

    Create news.js

---
+++ api/news.js
@@ -0,0 +1,15 @@
+export default async function handler(req, res) {
+  const API_KEY = process.env.NEWS_API_KEY;
+
+  try {
+    const response = await fetch(
+      `https://newsapi.org/v2/top-headlines?country=jp&pageSize=10&apiKey=${API_KEY}`
+    );
+
+    const data = await response.json();
+
+    res.status(200).json(data);
+  } catch (error) {
+    res.status(500).json({ error: "Failed to fetch news" });
+  }
+}
